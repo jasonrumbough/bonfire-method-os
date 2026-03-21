fdef81f969e55ad583b8ade69418452a1942645c
jasonrumbough <70698361+jasonrumbough@users.noreply.github.com>
2026-03-21 16:22:03 -0500

    Fix: add React import to AuditPage

--- src/pages/AuditPage.js
+++ src/pages/AuditPage.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { AUDIT_QUESTIONS, ASHES, SYSTEMS } from "../utils/data";
 import FireRating from "../components/StarRating";
